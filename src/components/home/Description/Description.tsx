71e87d4d6c39513e76c8d41b59c660f5d934a91c
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-30 15:55:26 -0500

    Cómo agregar archivos estáticos en Next.js.  Clase 14

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,17 @@
+import styles from "./Description.module.sass";
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Desciption</h1>
+    <section className={styles.Description}>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>
+          Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world
+          of cutting-edge gadgets and gear. Stay ahead of the curve and redefine
+          your digital lifestyle with us.
+        </p>
+      </div>
     </section>
   );
 };
