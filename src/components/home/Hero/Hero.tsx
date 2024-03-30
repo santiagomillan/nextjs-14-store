941e9301eaffffc9ebc840f7c66d8300be70c3c6
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-29 23:09:23 -0500

    CSS Modules en Next.js 13. Clase 10

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import styles from "./Hero.module.css";
+
 export const Hero = () => {
   return (
-    <section>
+    <section className={styles.Hero}>
       <h1>Hero</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
   );
 };
