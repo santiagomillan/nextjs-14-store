b2ae72f59a77ee006d1e135ba5b3d454c0026341
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-29 22:46:14 -0500

    Creación de arquitectura de landing page en Next.js. Clase 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 // "use client";
-import styles from "./page.module.css";
+
+import { Description } from "app/components/home/Description";
+import { Hero } from "app/components/home/Hero";
+import { MainProducts } from "app/components/home/MainProducts";
+
+// import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("hola mundo");
   return (
-    <main className={styles.main}>
-      <h1>Hola Mundo</h1>
+    <main>
+      <Hero />
+      <Description />
+      <MainProducts />
     </main>
   );
 }
---
+++ src/components/home/Description/Description.tsx
@@ -0,0 +1,7 @@
+export const Description = () => {
+  return (
+    <section>
+      <h1>Desciption</h1>
+    </section>
+  );
+};
