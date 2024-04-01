422c8bfae6e261d15f65f1563fa8e0876f1b4ca0
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-01 00:40:19 -0500

    implemetar interface en main products.

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,6 +1,45 @@
 import Image from "next/image";
 import styles from "./MainProducts.module.sass";
 
+interface Product {
+  id: number;
+  title: string;
+  body_html: string;
+  vendor: string;
+  product_type: string;
+  created_at: string;
+  handle: string;
+  updated_at: string;
+  published_at: string;
+  template_suffix: null | string;
+  published_scope: string;
+  tags: string;
+  status: string;
+  admin_graphql_api_id: string;
+  variants: Array<{
+    /* Detalles de la variante */
+  }>; // Necesitarías definir el tipo para las variantes
+  options: Array<{
+    /* Detalles de las opciones */
+  }>; // Necesitarías definir el tipo para las opciones
+  images: Array<{
+    /* Detalles de las imágenes */
+  }>; // Necesitarías definir el tipo para las imágenes
+  image: {
+    id: number;
+    alt: null | string;
+    position: number;
+    product_id: number;
+    created_at: string;
+    updated_at: string;
+    admin_graphql_api_id: string;
+    width: number;
+    height: number;
+    src: string;
+    variant_ids: number[];
+  };
+}
+
 const getProducts = async () => {
   try {
     const response = await fetch(
@@ -25,8 +64,8 @@ export const MainProducts = async () => {
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map((product: any) => {
-          const imageSrc = product.images[0].src;
+        {products?.map((product: Product) => {
+          const imageSrc = product.image.src;
           return (
             <article key={product.id}>
               <p>{product.title}</p>
