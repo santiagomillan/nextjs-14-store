b14780d45cdbb52dea923b00060682b01f67b3f5
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-01 00:36:40 -0500

    Manejo de errores en la UI con el archivo error.tsx. Clase 23

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <div style={{ padding: "10rem" }}>
+      <h1>Error</h1>
+      <p>Ha ocurrido un error</p>
+      <button onClick={reset}>Intentar de nuevo</button>
+    </div>
+  );
+}
