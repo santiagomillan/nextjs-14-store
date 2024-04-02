b0593ccefb6ad57bac76ccdc4188aa713319cf23
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-02 18:08:26 -0500

    Data fetching de parámetros en el servidor y cliente. Clase 32

---
+++ src/app/product/[handle]/page.tsx
@@ -0,0 +1,17 @@
+// interface ProductsPageprops {
+//   searchParams: {
+//     id: string;
+//   };
+// }
+"use client";
+
+import { useParams, useSearchParams } from "next/navigation";
+
+export default function ProductPage() {
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  console.log(params);
+  console.log(id);
+  return <h1>Product Page</h1>;
+}
