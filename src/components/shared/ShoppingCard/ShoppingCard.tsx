0eb1cb3d599a84131f7992c6c9557163ce8bd0d3
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-17 00:03:45 -0500

    Proyecto: agregando items al carrito de compras. Clase 47

--- src/components/shared/ShoppingCard/ShoppingCard.tsx
+++ src/components/shared/ShoppingCard/ShoppingCard.tsx
@@ -7,10 +7,23 @@ import { useShoppingCart } from "app/hooks/useShoppingCart";
 export const ShoppingCard = () => {
   const { cart } = useShoppingCart();
   console.log(cart);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>{cart.length}</span>
       <FaShoppingCart />
+      {isOpen && (
+        <div className={styles.ShoppingCart__items}>
+          {cart.map((item) => (
+            <>
+              <p key={item?.id}>{item?.title}</p>
+              <p>Cantidad: {item.quantity}</p>
+            </>
+          ))}
+          <button className={styles.ShoppingCart__buyButton}>Buy</button>
+        </div>
+      )}
     </button>
   );
 };
